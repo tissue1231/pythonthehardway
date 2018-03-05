d78cbe3887212754391ca168ebeda82b2180d0c7
tissue <tissue@uber.com>
2018-03-05 08:14:37 -0800

    why not one more

---
+++ temp/ex7.py
@@ -0,0 +1,26 @@
+# This prints the below as is
+print "Mary had a little lamb."
+#This prints the below and adds in the string "snow"
+print "Its fleece was white as %s." % 'snow'
+#this prints out the below text
+print "And everywhere that Mary went"
+#This prints out 10 periods
+print "." * 10 #what'd that do?
+#This is defining 12 strings
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+#this adds all the strings above into one and adds a space between chesse and burger with the comma
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
