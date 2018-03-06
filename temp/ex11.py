5344bedc20f93650bd885807dad29e06b13da8e0
tissue <tissue@uber.com>
2018-03-06 06:59:02 -0800

    here we go

--- temp/ex11.py
+++ temp/ex11.py
@@ -3,7 +3,7 @@ print "Whats you're favorite color?",
 color = raw_input()
 print "what do you smell like?",
 smell = raw_input()
-print "Hows the broccoli taste",
+print "Hows the broccoli taste?",
 taste = raw_input()
 
 print "So, you're favorite color is %r, you smell like %r and the broccoli %r testes bad." % (
