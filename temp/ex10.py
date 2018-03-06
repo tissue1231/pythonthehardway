50c6b4b0ba77748e1ab34f808c65dfbe73de83a9
tissue <tissue@uber.com>
2018-03-05 18:05:00 -0800

    some stuff

--- temp/ex10.py
+++ temp/ex10.py
@@ -5,12 +5,12 @@ persian_cat = "I'm split\non a line."
 # double\ puts backslashes between words
 backslash_cat = "I'm \\ a \\ cat."
 #""" lets you write a string with as many lines as you want
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat Food
 \t* fishies
-\t* Catnip\n\t* Grass #\n\t 
-"""
+\t* Catnip\n\t* Grass \n\t 
+'''
 #\n\t splist and tabs the line
 print tabby_cat
 print persian_cat
